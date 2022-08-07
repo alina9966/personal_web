5a6b2ad31ce3c0a4c6a1b58013d995217391b8d4
Zhang <zzfeng233@163.com>
2022-08-07 22:03:19 +0800

    导航条和文件上传功能皮毛完成

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from flask import Flask
-
+from flask import render_template
+from flask import redirect
+from flask import request
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template('index.html', title="Mao")
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        save_path = os.path.join(r"~/upload", secure_filename(f.filename))
+        f.save(save_path)
+        f.close()
+        return redirect('/upload')
+    return render_template('upload.html')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
+    app.run('127.0.0.1', 5000)
